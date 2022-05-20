2ecca16ccaacb0474058476a88d33c1346565b11
Arpit Tripathi <arpit04tripathi@gmail.com>
2022-05-20 18:46:21 +0100

    mode prod

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    port: 3000,
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-js-app/" : "/",
+});
